5f08c1533623c4521b8d402ae3bae5d6a4b10f5c
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-10-13 13:43:38 +0900

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,9 +17,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
+            <h1 class="text-4xl">{{ config('app.name', 'Laravel') }}</h1>
         </div>
     </div>
 </x-guest-layout>
