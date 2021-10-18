01d67485c4c314f5e443d75a62178c280b2af2f3
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-10-18 11:13:22 +0900

    new

--- resources/views/components/new-item.blade.php
+++ resources/views/components/new-item.blade.php
@@ -1,6 +1,6 @@
-<div class="bg-white w-52">
+<div class="bg-white w-full sm:w-56">
     <div class="flex justify-center border-4 hover:border-indigo-500 rounded-lg">
-        <img class="object-contain h-52" src="{{ empty($image) ? asset('images/01.png') : $image }}" alt="{{ $name }}" title="{{ $name }}">
+        <img class="object-contain h-full sm:h-56" src="{{ empty($image) ? asset('images/01.png') : $image }}" alt="{{ $name }}" title="{{ $name }}">
     </div>
     <span class="font-bold">{{ $name }}</span>
 </div>
