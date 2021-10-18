e687f75d347fdab33c143a8ac9e80d1e086b7dbb
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-10-18 14:46:28 +0900

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -15,39 +9,7 @@
         </div>
     </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="p-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <table class="table-auto w-full">
-                    <thead>
-                        <tr>
-                            <th>ファイル</th>
-                            <th>名前</th>
-                            <th>説明</th>
-                            <th>作成日</th>
-                            <th>更新日</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($materials as $material)
-                            <tr class="text-center">
-                                <td>
-                                    <img class="object-contain h-full sm:h-24 mx-auto" src="{{ route('file', $material) }}" loading="lazy">
-                                </td>
-
-                                <td>{{ $material->title }}</td>
-                                <td>{{ Str::limit($material->description, 20) }}</td>
-                                <td>{{ $material->created_at }}</td>
-                                <td>{{ $material->updated_at }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-
-            </div>
-            {{ $materials->links() }}
-        </div>
-    </div>
+    @include('material.list')
 
     {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
---
+++ resources/views/material/list.blade.php
@@ -0,0 +1,33 @@
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="p-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <table class="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th>ファイル</th>
+                        <th>名前</th>
+                        <th>説明</th>
+                        <th>作成日</th>
+                        <th>更新日</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($materials as $material)
+                        <tr class="text-center border-b border-indigo-500">
+                            <td>
+                                <img class="object-contain p-1 h-full sm:h-24 mx-auto" src="{{ route('file', $material) }}" loading="lazy">
+                            </td>
+
+                            <td>{{ $material->title }}</td>
+                            <td>{{ Str::limit($material->description, 20) }}</td>
+                            <td>{{ $material->created_at }}</td>
+                            <td>{{ $material->updated_at }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
+        {{ $materials->links() }}
+    </div>
+</div>
