3bd718debd500c76009a915d68026a7b716050f5
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-10-18 11:20:18 +0900

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,31 +9,10 @@
 
             <div class="w-auto h-64 rounded shadow-lg my-10 p-10 bg-indigo-500 text-white">この辺に大きいロゴ</div>
 
-            <h2 class="text-3xl">新着</h2>
+            @include('home.new')
 
-            <div class="m-1  flex flex-wrap gap-4">
+            @include('home.category')
 
-               <x-new-item :image="asset('images/01.png')" name="1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."></x-new-item>
-               <x-new-item :image="asset('images/02.png')" name="2"></x-new-item>
-               <x-new-item :image="asset('images/03.png')" name="3"></x-new-item>
-               <x-new-item :image="asset('images/04.png')" name="4"></x-new-item>
-               <x-new-item :image="asset('images/05.png')" name="5"></x-new-item>
-               <x-new-item :image="asset('images/06.png')" name="6"></x-new-item>
-               <x-new-item :image="asset('images/07.png')" name="7"></x-new-item>
-               <x-new-item :image="asset('images/08.png')" name="8"></x-new-item>
-               <x-new-item image="{{ $i ?? '' }}" name="9"></x-new-item>
-            </div>
-
-            <h2 class="text-3xl">カテゴリー</h2>
-
-            <div class="m-1 grid grid-flow-row grid-cols-3 grid-rows-3 gap-4">
-                <div class="w-auto h-40 bg-gray-100">1</div>
-                <div class="w-auto h-40 bg-gray-100">2</div>
-                <div class="w-auto h-40 bg-gray-100">3</div>
-                <div class="w-auto h-40 bg-gray-100">4</div>
-                <div class="w-auto h-40 bg-gray-100">5</div>
-                <div class="w-auto h-40 bg-gray-100">6</div>
-            </div>
         </div>
     </div>
 </x-main-layout>
---
+++ resources/views/home/category.blade.php
@@ -0,0 +1,10 @@
+<h2 class="text-3xl">カテゴリー</h2>
+
+<div class="m-1 grid grid-flow-row grid-cols-3 grid-rows-3 gap-4">
+    <div class="w-auto h-40 bg-gray-100">1</div>
+    <div class="w-auto h-40 bg-gray-100">2</div>
+    <div class="w-auto h-40 bg-gray-100">3</div>
+    <div class="w-auto h-40 bg-gray-100">4</div>
+    <div class="w-auto h-40 bg-gray-100">5</div>
+    <div class="w-auto h-40 bg-gray-100">6</div>
+</div>
