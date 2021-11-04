779fcbdeef1062e4e7a5b71e93ac377c3b3a1aaa
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-04 14:08:47 +0900

    title

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,8 @@
 <x-app-layout>
+    <x-slot name="title">
+        ダッシュボード
+    </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
--- resources/views/dashboard/materials/index.blade.php
+++ resources/views/dashboard/materials/index.blade.php
@@ -1,4 +1,7 @@
 <x-app-layout>
+    <x-slot name="title">
+        チーム素材
+    </x-slot>
 
     @include('dashboard.materials.list')
 
