0c5bba03e74b59ac5d9798dcb1612d37c712df62
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-06 12:36:33 +0900

    Update create.blade.php

--- resources/views/material/create.blade.php
+++ resources/views/material/create.blade.php
@@ -3,7 +3,7 @@
 <form action="{{ route('material.store') }}" method="post" enctype="multipart/form-data">
     @csrf
 
-    <x-jet-label for="file" value="{{ __('ファイル（必須。5MB以下。対応フォーマット：jpg,png）') }}" />
+    <x-jet-label for="file" value="{{ __('ファイル（必須。10MB以下。対応フォーマット：jpg,png）') }}" />
 
     <input type="file" name="file" required />
 
