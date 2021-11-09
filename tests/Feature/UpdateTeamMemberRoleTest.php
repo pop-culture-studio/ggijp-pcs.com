ef137c08b174d6423de32dcc4f8041aceed84c36
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-09 10:14:08 +0900

    Update UpdateTeamMemberRoleTest.php

--- tests/Feature/UpdateTeamMemberRoleTest.php
+++ tests/Feature/UpdateTeamMemberRoleTest.php
@@ -22,11 +22,11 @@ class UpdateTeamMemberRoleTest extends TestCase
 
         $component = Livewire::test(TeamMemberManager::class, ['team' => $user->currentTeam])
                         ->set('managingRoleFor', $otherUser)
-                        ->set('currentRole', 'editor')
+                        ->set('currentRole', 'member')
                         ->call('updateRole');
 
         $this->assertTrue($otherUser->fresh()->hasTeamRole(
-            $user->currentTeam->fresh(), 'editor'
+            $user->currentTeam->fresh(), 'member'
         ));
     }
 
@@ -42,7 +42,7 @@ class UpdateTeamMemberRoleTest extends TestCase
 
         $component = Livewire::test(TeamMemberManager::class, ['team' => $user->currentTeam])
                         ->set('managingRoleFor', $otherUser)
-                        ->set('currentRole', 'editor')
+                        ->set('currentRole', 'member')
                         ->call('updateRole')
                         ->assertStatus(403);
 
