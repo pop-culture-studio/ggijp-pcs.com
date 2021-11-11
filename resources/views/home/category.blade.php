2ea570ea2ab427d03a1baa91f4567af1cbb37b62
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-11 10:34:35 +0900

    Update category.blade.php

--- resources/views/home/category.blade.php
+++ resources/views/home/category.blade.php
@@ -3,7 +3,9 @@
 <x-admin-message text="ここのカテゴリーは固定カテゴリーと季節ごとの特集カテゴリーなどを配置。"/>
 
 <div class="my-6 mx-3 grid grid-flow-row grid-cols-3 grid-rows-3 gap-4">
-    <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AF%E3%83%AA%E3%82%B9%E3%83%9E%E3%82%B9"></div>
+    <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text={{ urlencode('クリスマス') }}"></div>
+    <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text={{ urlencode('正月') }}"></div>
+    <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC1"></div>
     <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC2"></div>
     <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC3"></div>
     <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC4"></div>
