9492d7a794bbd0a17b2bbdd56c29f1840854eb0d
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-11 10:26:35 +0900

    cat

--- resources/views/home/new.blade.php
+++ resources/views/home/new.blade.php
@@ -1,12 +1,12 @@
 <h2 class="text-3xl">新着</h2>
 
-<div class="my-6 flex flex-wrap gap-4">
+<div class="my-6 mx-3 flex flex-wrap gap-4">
     @foreach ($materials as $material)
         <x-item :material="$material" :image="$material->thumbnail" :name="$material->title"></x-item>
     @endforeach
 </div>
 
 <a href="{{ route('material.index') }}"
-    class="block text-center text-xl p-3 m-6 text-white bg-indigo-500 hover:bg-indigo-600 rounded-lg">
+    class="block text-center text-xl p-3 m-3 text-white bg-indigo-500 hover:bg-indigo-600 rounded-lg">
     すべての素材
 </a>
