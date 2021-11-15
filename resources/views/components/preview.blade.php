5d6570c4c36688ac10ca61ed838886944fc620ea
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-15 09:42:32 +0900

    Update preview.blade.php

--- resources/views/components/preview.blade.php
+++ resources/views/components/preview.blade.php
@@ -3,13 +3,13 @@
 <div class="flex justify-center rounded-lg p-5">
     @if (str_contains(Storage::mimeType($material->file), 'audio'))
         <audio controls src="{{ Storage::temporaryUrl($material->file, now()->addMinutes(60)) }}">
-            プレビュー
+            このブラウザでは表示できません。
         </audio>
     @elseif (str_contains(Storage::mimeType($material->file), 'video'))
         <video controls class="w-auto">
             <source src="{{ Storage::temporaryUrl($material->file, now()->addMinutes(60)) }}"
                 type="{{ Storage::mimeType($material->file) }}">
-            プレビュー
+            このブラウザでは表示できません。
         </video>
     @else
         <img class="object-contain h-full w-auto" src="{{ $material->thumbnail }}" title="{{ $material->title }}"
