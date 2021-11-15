36ae9f096362570fb50095e3c15f03807b37d9c8
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-15 09:49:28 +0900

    Update category.blade.php

--- resources/views/home/category.blade.php
+++ resources/views/home/category.blade.php
@@ -3,9 +3,11 @@
 {{-- <x-admin-message text="ここのカテゴリーは固定カテゴリーと季節ごとの特集カテゴリーなどを配置。" /> --}}
 
 <div class="my-6 mx-3 grid grid-flow-row grid-cols-3 grid-rows-3 gap-4">
-    <div class="w-auto"><img
-            src="https://placehold.jp/f71b1b/ffffff/350x180.png?text={{ urlencode('クリスマス') }}"></div>
-    <div class="w-auto"><img src="https://placehold.jp/f71b1b/ffffff/350x180.png?text={{ urlencode('正月') }}">
+    <div class="w-auto">
+        <img src="https://placehold.jp/9697a3/ffffff/350x180.png?text={{ urlencode('クリスマス') }}">
+    </div>
+    <div class="w-auto">
+        <img src="https://placehold.jp/9697a3/ffffff/350x180.png?text={{ urlencode('正月') }}">
     </div>
 
     @foreach (config('pcs.category') as $cat)
@@ -15,12 +17,5 @@
             </a>
         </div>
     @endforeach
-
-    {{-- <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC1"></div>
-    <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC2"></div>
-    <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC3"></div>
-    <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC4"></div>
-    <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC5"></div>
-    <div class="w-auto"><img src="https://placehold.jp/9697a3/ffffff/350x180.png?text=%E3%82%AB%E3%83%86%E3%82%B4%E3%83%AA%E3%83%BC6"></div> --}}
 
 </div>
