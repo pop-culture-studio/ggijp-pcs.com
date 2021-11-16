61716a7dd7d38933d80931e3ecdd74709bcc866c
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-16 14:35:48 +0900

    Update Material.php

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -38,21 +38,21 @@ class Material extends Model
 
         $mime = Storage::mimeType($this->file);
 
-        if (!str_contains($mime, 'image')) {
-            $type = match (true) {
-                str_contains($mime, 'image/') => 'イラスト',
-                str_contains($mime, 'video/') => '動画',
-                str_contains($mime, 'audio/') => '音声',
-                str_contains($mime, '/zip') => 'ZIP',
-                default => 'その他'
-            };
-
-            return 'https://placehold.jp/ffffff/6366F1/350x350.png?text=' .
-                urlencode($type)
-                //.'&css=%7B%22background%22%3A%22%20-webkit-gradient(linear%2C%20left%20top%2C%20left%20bottom%2C%20from(%236366F1)%2C%20to(%23ffffff))%22%7D'
-            ;
+        if (str_contains($mime, 'image/')) {
+            return Storage::temporaryUrl($this->file, now()->addMinutes(60));
         }
 
-        return Storage::temporaryUrl($this->file, now()->addMinutes(60));
+        $type = match (true) {
+            str_contains($mime, 'image/') => 'イラスト',
+            str_contains($mime, 'video/') => '動画',
+            str_contains($mime, 'audio/') => '音声',
+            str_contains($mime, '/zip') => 'ZIP',
+            default => 'その他'
+        };
+
+        return 'https://placehold.jp/ffffff/6366F1/350x350.png?text=' .
+            urlencode($type)
+            //.'&css=%7B%22background%22%3A%22%20-webkit-gradient(linear%2C%20left%20top%2C%20left%20bottom%2C%20from(%236366F1)%2C%20to(%23ffffff))%22%7D'
+        ;
     }
 }
