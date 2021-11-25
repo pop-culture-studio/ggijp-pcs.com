64769d123ae550a0bb8f2593c76ada503776d58b
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-25 11:39:23 +0900

    Update item.blade.php

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -1,8 +1,10 @@
 <div class="bg-white w-full sm:w-56">
     <a href="{{ route('material.show', $material) }}">
         <div class="flex justify-center border-4 hover:border-indigo-500 rounded-lg">
-            <img class="object-contain h-full sm:h-56" src="{{ empty($image) ? asset('images/01.png') : $image }}"
-                alt="{{ $name }}" title="{{ $name }}" width="350" height="350">
+            <img class="object-contain h-full sm:h-56"
+                 src="{{ empty($image) ? 'https://placehold.jp/ffffff/999999/350x350.png?text='.urlencode('Not Found') : $image }}"
+                 alt="{{ $name }}" title="{{ $name }}"
+                 width="350" height="350">
         </div>
         <span class="font-bold hover:text-indigo-500">{{ $name }}</span>
     </a>
