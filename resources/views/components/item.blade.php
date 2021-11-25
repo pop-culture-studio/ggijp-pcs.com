2713859f941e59c3ffe7b6c81cd852a4a11802be
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-25 11:24:33 +0900

    Update item.blade.php

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -2,7 +2,7 @@
     <a href="{{ route('material.show', $material) }}">
         <div class="flex justify-center border-4 hover:border-indigo-500 rounded-lg">
             <img class="object-contain h-full sm:h-56" src="{{ empty($image) ? asset('images/01.png') : $image }}"
-                alt="{{ $name }}" title="{{ $name }}" loading="lazy">
+                alt="{{ $name }}" title="{{ $name }}">
         </div>
         <span class="font-bold hover:text-indigo-500">{{ $name }}</span>
     </a>
