73e1e95b21892d8132cf5f73bbdc39879bd182bb
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-11-26 11:26:26 +0900

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Team;
 use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,12 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+
+        Gate::before(function ($user, $ability) {
+            return in_array(optional($user)->id, [
+                1,
+                2,
+            ]);
+        });
     }
 }
