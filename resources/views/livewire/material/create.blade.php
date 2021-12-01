a9a143c1d5ca256cbb1659a4f645dc209d48b7ed
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-01 10:54:53 +0900

    Update create.blade.php

--- resources/views/livewire/material/create.blade.php
+++ resources/views/livewire/material/create.blade.php
@@ -42,7 +42,7 @@
 
     <div class="my-2">
         <x-jet-label for="cat" value="{{ __('カテゴリー（必須。複数設定するには,で区切ってください。）') }}"/>
-        <x-jet-input name="cat" type="text" class="mt-1 block w-full sm:w-1/2" required wire:model="cat"/>
+        <x-jet-input name="cat" type="text" class="mt-1 block w-full sm:w-1/2" required wire:model.lazy="cat"/>
         <x-jet-input-error for="cat" class="mt-2"/>
 
         <div class="my-2 text-md">基本カテゴリー
@@ -55,13 +55,13 @@
 
     <div class="my-2">
         <x-jet-label for="title" value="{{ __('タイトル（省略時はファイル名）') }}"/>
-        <x-jet-input name="title" type="text" class="mt-1 block w-full sm:w-1/2" wire:model="title"/>
+        <x-jet-input name="title" type="text" class="mt-1 block w-full sm:w-1/2" wire:model.defer="title"/>
         <x-jet-input-error for="title" class="mt-2"/>
     </div>
 
     <div class="my-2">
         <x-jet-label for="description" value="{{ __('説明') }}"/>
-        <textarea name="description" rows="4" cols="40" wire:model="description"
+        <textarea name="description" rows="4" cols="40" wire:model.defer="description"
                   class="mt-1 block w-full sm:w-1/2 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"></textarea>
         <x-jet-input-error for="description" class="mt-2"/>
     </div>
