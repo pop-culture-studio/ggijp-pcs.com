85d0b02517d463bf2cc607d747921c7199ae6ff3
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-12-25 12:10:50 +0900

    font

--- resources/views/components/gradient-h2.blade.php
+++ resources/views/components/gradient-h2.blade.php
@@ -1,1 +1,1 @@
-<h2 {{ $attributes->class(['text-3xl sm:text-9xl text-white bg-gradient-to-r from-indigo-500 to-white p-6 rounded-lg']) }}>{{ $slot }}</h2>
+<h2 {{ $attributes->class(['text-3xl sm:text-9xl text-white bg-gradient-to-r from-indigo-500 to-white p-6 rounded-lg font-extrabold']) }}>{{ $slot }}</h2>
