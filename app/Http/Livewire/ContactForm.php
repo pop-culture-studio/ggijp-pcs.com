7121213cbc3b2ad54aeb24a5af59804d39be2eab
PCS <pcs.engineer.team@gmail.com>
2022-02-17 12:03:13 +0900

    Contact

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use App\Mail\ContactMail;
 use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
 
@@ -19,15 +21,24 @@ class ContactForm extends Component
         'body' => 'required',
     ];
 
+    public function ready(Request $request)
+    {
+        $this->name = $request->cookie('name', '');
+        $this->email = $request->cookie('email', '');
+    }
+
     public function sendmail()
     {
         $this->validate();
 
         Contact::forceCreate([
-            'name'  => $this->name,
+            'name' => $this->name,
             'email' => $this->email,
-            'body'  => trim($this->body),
+            'body' => trim($this->body),
         ]);
+
+        Cookie::queue('name', $this->name, 60 * 24 * 30);
+        Cookie::queue('email', $this->email, 60 * 24 * 30);
 
         Mail::to(config('pcs.contact.mail'))
             ->send(new ContactMail($this->name, $this->email, $this->body));
--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -1,4 +1,5 @@
-<div class="w-full sm:max-w-xl mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+<div class="w-full sm:max-w-xl mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+     wire:init="ready">
     @if(session()->missing('mail_success'))
 
         <x-jet-validation-errors class="mb-4"/>
