aed27c0367a65f29610475a9500c8432ed0f36d0
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-03-07 10:38:44 +0900

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
 
-        {!! htmlScriptTagJsApi() !!}
+        {!! ReCaptcha::htmlScriptTagJsApi() !!}
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
