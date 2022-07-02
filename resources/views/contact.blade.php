8283ba02e40b34cd495a881312dab7b13721c523
PCS <pcs.engineer.team@gmail.com>
2022-07-02 15:02:36 +0900

    contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,11 +4,11 @@
     </x-slot>
 
     <div class="py-6">
-        <div class="sm:px-6 lg:px-8">
-            <h1 class="text-4xl mt-10">{{ __('お問い合わせ') }}</h1>
+        <div class="px-2 sm:px-6 lg:px-8">
+            <h1 class="text-4xl mt-10 text-center">{{ __('お問い合わせ') }}</h1>
 
-            <div class="py-3">ご要望やご質問、個人的なご依頼、サイトやデータについてお気づきの点などありましたらこちらよりご連絡ください。
-                素材のご利用については事前に｢利用規約｣｢よくある質問｣をご確認の上お願いします。
+            <div class="py-3 text-center sm:max-w-xl mx-auto">ご要望やご質問、個人的なご依頼、サイトやデータについてお気づきの点などありましたらこちらよりご連絡ください。
+                素材のご利用については事前に｢<a href="{{ route('terms.show') }}" class="text-indigo-500">利用規約</a>｣｢<a href="{{ route('faq') }}" class="text-indigo-500">よくある質問</a>｣をご確認の上お願いします。
                 お返事不要の場合はメッセージフォームからもお送りいただけます。
             </div>
 
--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full sm:max-w-xl mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+<div class="w-full sm:max-w-xl sm:mx-auto mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
      wire:init="ready">
     @if(session()->missing('mail_success'))
 
