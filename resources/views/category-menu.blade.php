3825ef934949d4d047e13418f0a97161f8e5ab61
PCS <pcs.engineer.team@gmail.com>
2022-07-02 19:57:01 +0900

    wip

--- resources/views/category-menu.blade.php
+++ resources/views/category-menu.blade.php
@@ -1,7 +1,7 @@
 <div class="my-6 mx-0 sm:mx-6 grid grid-flow-col gap-4">
     @foreach (config('pcs.category') as $cat)
         <div class="w-auto">
-            <a href="{{ route('category.show', Arr::get($cat, 'id')) }}" title="{{ Arr::get($cat, 'title') }}" class="hover:opacity-80">
+            <a href="{{ route('category.show', Arr::get($cat, 'title')) }}" title="{{ Arr::get($cat, 'title') }}" class="hover:opacity-80">
                 <img src="{{ asset('images/cat/' . Arr::get($cat, 'image')) }}" alt="{{ Arr::get($cat, 'title') }}" class="h-10 sm:h-20 mx-auto">
                 <div class="whitespace-nowrap font-bold mt-2 text-center {{ Arr::get($cat, 'color') }}">{{ Arr::get($cat, 'title') }}</div>
             </a>
--- resources/views/components/gradient-h2.blade.php
+++ resources/views/components/gradient-h2.blade.php
@@ -1,1 +1,1 @@
-<h2 {{ $attributes->class(['text-2xl sm:text-4xl text-gray-500 text-center p-6 rounded-lg font-extrabold']) }}>{{ $slot }}</h2>
+<h2 {{ $attributes->class(['text-2xl sm:text-4xl text-gray-500 text-center p-6 rounded-lg font-bold']) }}>{{ $slot }}</h2>
