f6df602da75a990f26d9619431e33e89b1decefd
PCS <pcs.engineer.team@gmail.com>
2022-07-04 09:33:17 +0900

    Update category.blade.php

--- resources/views/footer/category.blade.php
+++ resources/views/footer/category.blade.php
@@ -3,12 +3,14 @@
         カテゴリーから探す</h2>
 
     @foreach(config('pcs.keywords') as $key => $cats)
-        <div class="mx-auto py-6 sm:px-36 text-gray-500">
+        <div class="mx-auto py-6 px-6 sm:px-36 text-gray-500">
             <h3 class="text-xl font-bold">{{ $key }}</h3>
             <ul role="list" class="flex flex-wrap gap-3 justify-start whitespace-nowrap">
                 @foreach($cats as $cat)
                     <li>
-                        <a href="{{ route('category.show',$cat) }}" class="hover:underline">#{{ $cat }}</a>
+                        <a href="{{ route('category.show', $cat) }}" class="hover:underline">
+                            #{{ $cat }}
+                        </a>
                     </li>
                 @endforeach
             </ul>
