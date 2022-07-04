97272817567b0525d7b9f7f82d1677c3859a1c64
PCS <pcs.engineer.team@gmail.com>
2022-07-04 09:36:08 +0900

    Update category.blade.php

--- resources/views/footer/category.blade.php
+++ resources/views/footer/category.blade.php
@@ -1,6 +1,6 @@
 <div>
     <h2 class="text-2xl sm:text-4xl font-bold text-center text-gray-500" id="category">
-        カテゴリーから探す</h2>
+        キーワードから探す</h2>
 
     @foreach(config('pcs.keywords') as $key => $cats)
         <div class="mx-auto py-6 px-6 sm:px-36 text-gray-500">
