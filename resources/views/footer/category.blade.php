5e7c2e85de04e7b097efcd037d22c263c300e9b1
PCS <pcs.engineer.team@gmail.com>
2022-07-04 09:31:26 +0900

    add footer.category

---
+++ resources/views/footer/category.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <h2 class="text-2xl sm:text-4xl font-bold text-center text-gray-500" id="category">
+        カテゴリーから探す</h2>
+
+    @foreach(config('pcs.keywords') as $key => $cats)
+        <div class="mx-auto py-6 sm:px-36 text-gray-500">
+            <h3 class="text-xl font-bold">{{ $key }}</h3>
+            <ul role="list" class="flex flex-wrap gap-3 justify-start whitespace-nowrap">
+                @foreach($cats as $cat)
+                    <li>
+                        <a href="{{ route('category.show',$cat) }}" class="hover:underline">#{{ $cat }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endforeach
+</div>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,8 @@
 <footer class="mt-10">
     @include('footer.all')
 
+    @include('footer.category')
+
     @include('footer.sns')
 
     @include('footer.about')
