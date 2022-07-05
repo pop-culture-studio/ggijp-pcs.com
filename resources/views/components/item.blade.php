b196813f93bd99b2a45ab53b898d1e64799beff9
PCS <pcs.engineer.team@gmail.com>
2022-07-05 09:59:35 +0900

    Update item.blade.php

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -1,7 +1,7 @@
-<div class="bg-white w-full sm:w-56">
+<div class="bg-white w-full sm:w-56 text-center">
     <a href="{{ route('material.show', $material) }}">
         <div class="flex justify-center border-4 hover:border-indigo-500 rounded-lg sm:rounded-full overflow-hidden">
-            <img class="object-contain h-full sm:h-56"
+            <img class="object-cover h-full sm:h-56"
                  src="{{ empty($image) ? 'https://placehold.jp/ffffff/999999/350x350.png?text='.urlencode('Not Found') : $image }}"
                  alt="{{ $name }}" title="{{ $name }}"
                  width="350" height="350">
