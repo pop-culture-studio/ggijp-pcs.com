47a5d24a7ee013a25dd1b4532d14a057121b746a
PCS <pcs.engineer.team@gmail.com>
2022-07-05 15:14:10 +0900

    wip

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -1,7 +1,7 @@
-<div class="bg-white w-80 sm:w-56 text-center mx-auto sm:mx-0">
+<div class="bg-white w-72 sm:w-56 text-center mx-auto sm:mx-0">
     <a href="{{ route('material.show', $material) }}">
         <div class="flex justify-center border-4 hover:border-indigo-500 rounded-full sm:rounded-full overflow-hidden">
-            <img class="object-cover h-80 sm:h-56"
+            <img class="object-cover h-72 sm:h-56"
                  src="{{ empty($image) ? 'https://placehold.jp/ffffff/999999/350x350.png?text='.urlencode('Not Found') : $image }}"
                  alt="{{ $name }}" title="{{ $name }}"
                  width="350" height="350">
