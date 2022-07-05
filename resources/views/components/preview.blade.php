34ea4f2750c414e58aa4e3bbf7835ac90243f72f
PCS <pcs.engineer.team@gmail.com>
2022-07-05 10:46:53 +0900

    Update preview.blade.php

--- resources/views/components/preview.blade.php
+++ resources/views/components/preview.blade.php
@@ -2,15 +2,18 @@
 
 <div class="flex justify-center rounded-lg p-5">
     @if (str_contains(Storage::mimeType($material->file), 'audio'))
-        <audio controls src="{{ Storage::temporaryUrl($material->file, now()->addMinutes(60)) }}">
+        <audio controls src="{{ Storage::temporaryUrl($material->file, now()->addHours(12)) }}">
             このブラウザでは表示できません。
         </audio>
     @elseif (str_contains(Storage::mimeType($material->file), 'video'))
         <video controls class="w-auto">
-            <source src="{{ Storage::temporaryUrl($material->file, now()->addMinutes(60)) }}"
+            <source src="{{ Storage::temporaryUrl($material->file, now()->addHours(12)) }}"
                 type="{{ Storage::mimeType($material->file) }}">
             このブラウザでは表示できません。
         </video>
+    @elseif (str_contains(Storage::mimeType($material->file), 'image'))
+        <img class="object-contain h-full w-auto" src="{{ Storage::temporaryUrl($material->file, now()->addHours(12)) }}" title="{{ $material->title }}"
+             alt="{{ $material->title }}">
     @else
         <img class="object-contain h-full w-auto" src="{{ $material->image }}" title="{{ $material->title }}"
             alt="{{ $material->title }}">
