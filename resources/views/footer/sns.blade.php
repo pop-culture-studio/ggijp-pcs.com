e410280fc314c8ff05c4717eb5740de9d127af37
PCS <pcs.engineer.team@gmail.com>
2022-07-07 10:31:49 +0900

    Update sns.blade.php

--- resources/views/footer/sns.blade.php
+++ resources/views/footer/sns.blade.php
@@ -1,6 +1,16 @@
 <div class="text-center">
     <div class="text-3xl">SNSでシェア！</div>
-    <div>
-        Twitter Facebook
+    <div class="flex justify-center gap-2 mt-3">
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-via="PCS_miraizu"
+           data-hashtags="未来図倉庫" data-related="PCS_miraizu" data-show-count="false">Tweet</a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+        <div class="line-it-button" data-lang="ja" data-type="share-a" data-env="REAL"
+             data-url="https://developers.line.biz/ja/docs/line-social-plugins/install-guide/using-line-share-buttons/"
+             data-color="default" data-size="small" data-count="false" data-ver="3" style="display: none;"></div>
+        <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async"
+                defer="defer"></script>
+
+        <a href="https://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="basic-label-counter" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
     </div>
 </div>
