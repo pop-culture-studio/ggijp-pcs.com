c2aa459c57ccdae449b3d9669e57f45b648472eb
PCS <pcs.engineer.team@gmail.com>
2022-07-07 11:16:45 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,38 +11,36 @@
         </x-ogp>
     </x-slot>
 
-    <div class="py-0">
-        <div>
-            <h1 class="text-4xl hidden">{{ config('app.name', 'Laravel') }}</h1>
+    <div>
+        <h1 class="text-4xl hidden">{{ config('app.name', 'Laravel') }}</h1>
 
-            <div class="w-auto h-auto mt-10 bg-white relative">
-                <img src="{{ asset('/images/bg_home.png') }}">
+        <div class="w-auto h-auto mt-10 bg-white relative">
+            <img src="{{ asset('/images/bg_home.png') }}">
 
-                <div class="w-1/2 absolute top-1/3 left-1/4 right-1/4 z-30">
-                    <a href="{{ route('home') }}">
-                        <img src="{{ asset('images/logo_home.png') }}"
-                             alt="{{ config('app.name', 'Laravel') }}"
-                             title="{{ config('app.name', 'Laravel') }}"
-                        >
-                    </a>
-                </div>
+            <div class="w-1/2 absolute top-1/3 left-1/4 right-1/4 z-30">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/logo_home.png') }}"
+                         alt="{{ config('app.name', 'Laravel') }}"
+                         title="{{ config('app.name', 'Laravel') }}"
+                    >
+                </a>
             </div>
+        </div>
 
-            @includeIf('search')
+        @includeIf('search')
 
-            @includeIf('category-menu')
+        @includeIf('category-menu')
 
-            <div class="sm:px-6 lg:px-8">
+        <div class="sm:px-6 lg:px-8">
 
-                @if($popular_materials)
-                    @include('home.popular')
-                @endif
+            @if($popular_materials)
+                @include('home.popular')
+            @endif
 
-                @if($new_materials)
-                    @include('home.new')
-                @endif
+            @if($new_materials)
+                @include('home.new')
+            @endif
 
-            </div>
         </div>
     </div>
 </x-main-layout>
