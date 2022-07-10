6a495c36d804b73ecc364e786d880dab6cc78a78
PCS <pcs.engineer.team@gmail.com>
2022-07-10 09:04:41 +0900

    Update all.blade.php

--- resources/views/footer/all.blade.php
+++ resources/views/footer/all.blade.php
@@ -1,4 +1,6 @@
-<a href="{{ route('material.index') }}"
-   class="w-fit mx-auto block text-center text-xl py-3 px-16 m-6 text-white bg-indigo-500 hover:bg-indigo-400 rounded-full">
-    すべての素材を見る
-</a>
+@unless(! request()->routeIs('contact') && ! request()->routeIs('request') )
+    <a href="{{ route('material.index') }}"
+       class="w-fit mx-auto block text-center text-xl py-3 px-16 m-6 text-white bg-indigo-500 hover:bg-indigo-400 rounded-full">
+        すべての素材を見る
+    </a>
+@endunless
