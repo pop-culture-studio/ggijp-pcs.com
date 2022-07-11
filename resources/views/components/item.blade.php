640c3a8fdd4970ee8233a2a3de96146ec579843d
PCS <pcs.engineer.team@gmail.com>
2022-07-11 16:50:49 +0900

    category

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -1,7 +1,7 @@
-<div class="bg-white dark:bg-black w-72 sm:w-56 text-center mx-auto sm:mx-0">
+<div class="bg-white dark:bg-black w-72 sm:w-56 text-center mx-auto sm:mx-0 relative">
     <a href="{{ route('material.show', $material) }}">
-        <span class="font-bold text-{{ $material->categoryColor }}">{{ $material->categories()->first()?->name ?? '' }}</span>
-        <div class="dark:bg-gray-800 flex justify-center ring-2 ring-indigo-300 hover:ring-indigo-500 shadow-xl rounded-full overflow-hidden">
+        <div class="text-sm font-bold grid place-content-center h-14 w-14 bg-white dark:bg-indigo-800 absolute left-0 top-0 rounded-full ring-2 ring-{{ $material->categoryColor }} text-{{ $material->categoryColor }}">{{ $material->categories()->first()?->name ?? '' }}</div>
+        <div class="dark:bg-gray-800 flex justify-center ring-2 ring-{{ $material->categoryColor }} hover:ring-4 shadow-lg rounded-full overflow-hidden">
             <img class="object-cover h-72 sm:h-56"
                  src="{{ empty($image) ? 'https://placehold.jp/ffffff/999999/350x350.png?text='.urlencode('Not Found') : $image }}"
                  alt="{{ $name }}" title="{{ $name }}"
@@ -10,3 +10,4 @@
         <span class="font-bold hover:text-indigo-500">{{ $name }}</span>
     </a>
 </div>
+
