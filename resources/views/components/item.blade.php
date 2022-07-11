0a9d952bf646a5c82672b1fb60b28eca123874b6
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-07-11 10:13:55 +0900

    wip

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white w-72 sm:w-56 text-center mx-auto sm:mx-0">
     <a href="{{ route('material.show', $material) }}">
-        <div class="flex justify-center border-4 hover:border-indigo-500 rounded-full sm:rounded-full overflow-hidden">
+        <div class="flex justify-center ring-2 ring-indigo-300 hover:ring-indigo-500 shadow-xl rounded-full overflow-hidden">
             <img class="object-cover h-72 sm:h-56"
                  src="{{ empty($image) ? 'https://placehold.jp/ffffff/999999/350x350.png?text='.urlencode('Not Found') : $image }}"
                  alt="{{ $name }}" title="{{ $name }}"
