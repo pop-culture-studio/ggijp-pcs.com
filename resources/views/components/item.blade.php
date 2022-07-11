f642d16e7056fc38fa913308394bb2126663d3a0
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-07-11 12:01:14 +0900

    Update item.blade.php

--- resources/views/components/item.blade.php
+++ resources/views/components/item.blade.php
@@ -1,5 +1,6 @@
 <div class="bg-white dark:bg-black w-72 sm:w-56 text-center mx-auto sm:mx-0">
     <a href="{{ route('material.show', $material) }}">
+        <span class="font-bold text-{{ $material->categoryColor }}">{{ $material->categories()->first()->name ?? '' }}</span>
         <div class="dark:bg-gray-800 flex justify-center ring-2 ring-indigo-300 hover:ring-indigo-500 shadow-xl rounded-full overflow-hidden">
             <img class="object-cover h-72 sm:h-56"
                  src="{{ empty($image) ? 'https://placehold.jp/ffffff/999999/350x350.png?text='.urlencode('Not Found') : $image }}"
