ca418f92a03df19a00a7ae56c2c24e75a8666961
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-07-15 11:32:18 +0900

    Update gallery.blade.php

--- resources/views/livewire/material/gallery.blade.php
+++ resources/views/livewire/material/gallery.blade.php
@@ -1,6 +1,6 @@
 <div wire:init="zip">
     @if($files)
-        <div class="grid grid-cols-3 gap-3">
+        <div class="grid grid-cols-3 gap-3 bg-indigo-100 dark:bg-indigo-600 p-3 rounded-lg">
             @foreach($files as $name => $file)
                 <img src="{{ $file['image'] }}"
                      loading="lazy"
