803df46e9b7c70bd44d027da2175293cbaac0e37
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-07-15 10:30:56 +0900

    zip

--- resources/views/livewire/material/gallery.blade.php
+++ resources/views/livewire/material/gallery.blade.php
@@ -2,7 +2,7 @@
     @if($files)
         <div class="grid grid-cols-2 gap-3">
             @foreach($files as $name => $file)
-                <img src="data:{{ $file['mime'] }};base64,{{ $file['data'] }}"
+                <img src="data:{!! $file['mime'] !!};base64,{!! $file['data'] !!}"
                      alt="{{ $name }}" title="{{ $name }}">
             @endforeach
         </div>
