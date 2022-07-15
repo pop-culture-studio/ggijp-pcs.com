655179e02e6672487eff032f2bf501f6ed2d4d4e
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-07-15 10:45:53 +0900

    zip

--- resources/views/livewire/material/gallery.blade.php
+++ resources/views/livewire/material/gallery.blade.php
@@ -1,9 +1,10 @@
 <div wire:init="zip">
     @if($files)
-        <div class="grid grid-cols-2 gap-3">
+        <div class="grid grid-cols-3 gap-3">
             @foreach($files as $name => $file)
                 <img src="{{ $file['image'] }}"
-                     alt="{{ $name }}" title="{{ $name }}">
+                     alt="{{ $name }}" title="{{ $name }}"
+                class="pcs:scale-up-image">
             @endforeach
         </div>
     @endif
