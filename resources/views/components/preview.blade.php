47687f261c987ffa8a4038c6ae1038fb3108de08
PCS <pcs.engineer.team@gmail.com>
2022-07-27 08:24:14 +0900

    audio preview

--- resources/views/components/preview.blade.php
+++ resources/views/components/preview.blade.php
@@ -1,21 +1,27 @@
 @props(['material'])
 
-<div class="flex justify-center rounded-lg sm:p-5">
+<div class="flex justify-center rounded-lg sm:p-5 sm:mb-10">
     @if (str_contains(Storage::mimeType($material->file), 'audio'))
-        <audio controls src="{{ Storage::temporaryUrl($material->file, now()->addHours(12)) }}">
-            このブラウザでは表示できません。
-        </audio>
+        <div>
+            <img class="object-contain h-full w-auto" src="{{ $material->image }}" title="{{ $material->title }}"
+                 alt="{{ $material->title }}">
+
+            <audio controls src="{{ Storage::temporaryUrl($material->file, now()->addHours(12)) }}">
+                このブラウザでは表示できません。
+            </audio>
+        </div>
     @elseif (str_contains(Storage::mimeType($material->file), 'video'))
         <video controls class="w-auto">
             <source src="{{ Storage::temporaryUrl($material->file, now()->addHours(12)) }}"
-                type="{{ Storage::mimeType($material->file) }}">
+                    type="{{ Storage::mimeType($material->file) }}">
             このブラウザでは表示できません。
         </video>
     @elseif (str_contains(Storage::mimeType($material->file), 'image'))
-        <img class="object-contain h-full w-auto" src="{{ Storage::temporaryUrl($material->file, now()->addHours(12)) }}" title="{{ $material->title }}"
+        <img class="object-contain h-full w-auto"
+             src="{{ Storage::temporaryUrl($material->file, now()->addHours(12)) }}" title="{{ $material->title }}"
              alt="{{ $material->title }}">
     @else
         <img class="object-contain h-full w-auto" src="{{ $material->image }}" title="{{ $material->title }}"
-            alt="{{ $material->title }}">
+             alt="{{ $material->title }}">
     @endif
 </div>
