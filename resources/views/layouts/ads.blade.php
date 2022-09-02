1e791999f593d18a1c9b5caa124e428e42b9b48c
PCS <pcs.engineer.team@gmail.com>
2022-09-02 11:17:49 +0900

    Update ads.blade.php

--- resources/views/layouts/ads.blade.php
+++ resources/views/layouts/ads.blade.php
@@ -1,2 +1,4 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3717819644250147"
-        crossorigin="anonymous"></script>
+@guest
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3717819644250147"
+            crossorigin="anonymous"></script>
+@endguest
