776b6ce00a019ac7ef7f246b3a0446b02781be84
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-11-15 11:29:34 +0900

    Update popular.blade.php

--- resources/views/home/popular.blade.php
+++ resources/views/home/popular.blade.php
@@ -1,6 +1,6 @@
 <x-h2 class="mt-20 mb-10" id="popular">人気な素材</x-h2>
 
-<div class="my-6 mx-3 flex flex-wrap gap-4">
+<div class="my-6 mx-3 flex flex-wrap gap-4 justify-center">
     @foreach ($popular_materials as $material)
         <x-item :material="$material" :image="$material->image" :name="$material->title"></x-item>
     @endforeach
