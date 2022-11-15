a390bcfb906162028f34ff17e5b56dd99601662d
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-11-15 11:29:31 +0900

    Update new.blade.php

--- resources/views/home/new.blade.php
+++ resources/views/home/new.blade.php
@@ -1,6 +1,6 @@
 <x-h2 class="mt-20 mb-10" id="new">新しい素材</x-h2>
 
-<div class="my-6 mx-3 flex flex-wrap gap-4">
+<div class="my-6 mx-3 flex flex-wrap gap-4 justify-center">
     @foreach ($new_materials as $material)
         <x-item :material="$material" :image="$material->image" :name="$material->title"></x-item>
     @endforeach
