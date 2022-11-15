9a51d255e5e9820b39e82f553b90d568b8de53fa
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-11-15 14:26:03 +0900

    wip

--- resources/views/footer/category.blade.php
+++ resources/views/footer/category.blade.php
@@ -5,7 +5,7 @@
 
     @foreach(config('category') as $name => $cats)
         <div class="mx-auto py-6 px-6 sm:px-36 text-gray-500 dark:text-white">
-            <h3 class="text-xl font-bold">
+            <h3 class="text-xl font-bold text-gray-800">
                 <a href="{{ route('category.show', $name) }}" class="hover:underline">
                     {{ $name }}
                 </a>
