5f5e2c98ef9617e3469aefc61725ed77fd7541aa
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-11-15 13:35:55 +0900

    wip

--- resources/views/components/category.blade.php
+++
@@ -1,5 +0,0 @@
-<a href="{{ $url }}"
-    class="text-md text-black bg-white font-bold border border-indigo-500 shadow-sm py-1 px-2 rounded-lg hover:border-indigo-800">
-    {{ $name }}
-    @isset($count) <span class="text-xs font-normal text-gray-600">[{{ $count }}]</span> @endisset
-</a>
