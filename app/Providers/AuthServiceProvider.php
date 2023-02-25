0090fc220e072c218862d90d7e36d43f03e32581
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-02-25 10:53:52 +0900

    Laravel 10

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,8 +26,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
         Gate::define('admin', function (User $user) {
             return $user->hasTeamPermission(Team::find(config('pcs.team_id')), 'admin');
         });
