76ce1268f6d110a7c729c17a1c41043ae46d189c
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-03-08 11:10:58 +0900

    wip

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -13,7 +13,7 @@ class ViewServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
@@ -23,7 +23,7 @@ class ViewServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         View::composer('home', HomeComposer::class);
     }
