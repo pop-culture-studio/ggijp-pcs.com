b1646f9346f2efbd0ddb5505a6a6a941e4ddbdf3
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-03-25 14:55:42 +0900

    Update CategoryTest.php

--- tests/Feature/Material/CategoryTest.php
+++ tests/Feature/Material/CategoryTest.php
@@ -29,7 +29,7 @@ class CategoryTest extends TestCase
         $response->assertSuccessful()
                  ->assertViewHasAll([
                      'category' => $cat,
-                     'materials'
+                     'materials',
                  ]);
     }
 }
