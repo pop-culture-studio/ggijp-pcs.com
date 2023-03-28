b96b96fdcb42552f2037e3f75865177d375e609b
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-03-28 11:26:10 +0900

    Update ChatJob.php

--- app/Jobs/ChatJob.php
+++ app/Jobs/ChatJob.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use App\Models\Category;
-use App\Models\Material;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
