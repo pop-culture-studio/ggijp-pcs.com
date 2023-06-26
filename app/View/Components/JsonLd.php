b794225032c55367e9af1f405d828fde9e0e3c78
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-06-26 13:24:53 +0900

    Update JsonLd.php

--- app/View/Components/JsonLd.php
+++ app/View/Components/JsonLd.php
@@ -7,7 +7,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 use JsonLd\Context;
-use JsonLd\ContextTypes\CreativeWork;
+use App\Support\JsonLd\ImageObject;
 use JsonLd\ContextTypes\Person;
 
 class JsonLd extends Component
@@ -26,16 +26,15 @@ class JsonLd extends Component
      */
     public function render(): View|Closure|string
     {
-        $context = Context::create(CreativeWork::class, [
-            'description' => $this->material->description,
-            'name' => $this->material->title,
-            'url' => route('material.show', $this->material),
-            'author' => new Person([
-                'name' => $this->material->author,
+        $name = empty($this->material->author) ? config('app.name') : $this->material->author;
+
+        $context = Context::create(ImageObject::class, [
+            'creator' => new Person([
+                'name' => $name,
             ]),
-            'dateCreated' => $this->material->created_at,
-            'dateModified' => $this->material->updated_at,
-            'thumbnailUrl' => $this->material->image,
+            'creditText' => config('app.name'),
+            'contentUrl' => $this->material->image,
+            'license' => route('terms.show'),
         ]);
 
         return view('components.json-ld')->with(compact('context'));
