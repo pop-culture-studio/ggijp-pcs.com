de94051630889705fab9c53711a04d81ceb38486
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-08-30 17:02:02 +0900

    Update preview.blade.php

--- resources/views/components/preview.blade.php
+++ resources/views/components/preview.blade.php
@@ -3,7 +3,7 @@
 <div class="flex justify-center rounded-lg sm:p-5 sm:mb-10">
     @if (str_contains(Storage::mimeType($material->file), 'audio'))
         <div class="mb-3 sm:mb-0">
-            <audio class="w-full sm:mb-3" controls
+            <audio class="w-full sm:mb-3" controls controlslist="nodownload"
                    src="{{ Storage::temporaryUrl($material->file, now()->addHours(12)) }}">
                 このブラウザでは表示できません。
             </audio>
