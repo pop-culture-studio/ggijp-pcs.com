d2becf0ccf8fb1e770ba8d533489b8e08ad07d1d
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-09-04 11:03:10 +0900

    Update livewire.php

--- config/livewire.php
+++ config/livewire.php
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'legacy_model_binding' => true,
+    'legacy_model_binding' => false,
 
     /*
     |---------------------------------------------------------------------------
