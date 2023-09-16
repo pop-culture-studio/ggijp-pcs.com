e72df3d5061e9e2570b74b2ecc068a15c42d0d5c
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-09-16 11:04:28 +0900

    Update UpdateChat.php

--- app/Livewire/Material/UpdateChat.php
+++ app/Livewire/Material/UpdateChat.php
@@ -4,7 +4,6 @@ namespace App\Livewire\Material;
 
 use App\Jobs\ChatJob;
 use App\Models\Category;
-use Illuminate\View\View;
 use Livewire\Component;
 
 class UpdateChat extends Component
@@ -16,10 +15,5 @@ class UpdateChat extends Component
         ChatJob::dispatch($this->category);
 
         session()->flash('message', 'ChatGPTによる説明を更新中。少し待ってからリロードしてください。');
-    }
-
-    public function render(): View
-    {
-        return view('livewire.material.update-chat');
     }
 }
