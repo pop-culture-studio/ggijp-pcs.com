046ce8c0392958478cddff156c318e973c17bae5
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-11-13 18:48:57 +0900

    Update about.blade.php

--- resources/views/footer/about.blade.php
+++ resources/views/footer/about.blade.php
@@ -13,7 +13,7 @@
             <a href="{{ route('form.contact') }}" class="hover:underline">{{ __('リクエスト・お問い合わせ') }}</a>
         </li>
         <li>
-            <a href="https://chat.openai.com/g/g-NBBKTeE79-wei-lai-tu-cang-ku" class="hover:underline">GPTs</a>
+            <a href="https://chat.openai.com/g/g-NBBKTeE79-wei-lai-tu-cang-ku" class="hover:underline" target="_blank">GPTs</a>
         </li>
         <li>
             <a href="{{ route('policy.show') }}" class="hover:underline">{{ __('プライバシーポリシー') }}</a>
