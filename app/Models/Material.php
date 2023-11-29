2138bad4b66269aab509b43a6d4f267313921da2
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-11-30 08:38:11 +0900

    indexnow

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -6,6 +6,7 @@ use App\Casts\Material\CategoryColor;
 use App\Casts\Material\Image;
 use App\Models\Concerns\MaterialFeed;
 use App\Models\Concerns\MaterialScope;
+use App\Support\IndexNow;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -13,6 +14,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Feed\Feedable;
+
+use function Illuminate\Events\queueable;
 
 /**
  * @mixin IdeHelperMaterial
@@ -57,25 +60,27 @@ class Material extends Model implements Feedable
      */
     protected $with = ['categories'];
 
-    /**
-     * @return BelongsTo
-     */
+    protected static function booted(): void
+    {
+        static::created(queueable(function (Material $material) {
+            IndexNow::submit(route('material.show', $material));
+        }));
+
+        static::updated(queueable(function (Material $material) {
+            IndexNow::submit(route('material.show', $material));
+        }));
+    }
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    /**
-     * @return BelongsToMany
-     */
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class);
     }
 
-    /**
-     * @return Attribute
-     */
     public function mainCategory(): Attribute
     {
         return Attribute::make(
