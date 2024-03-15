b7f4e7c929a8eba9eab2a0486e9b16fb57f7e1c4
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-03-15 10:26:57 +0900

    Laravel11

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
 
     public function setUp(): void
     {
